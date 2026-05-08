a47a1a471047bf575d3d79767d47a62410a3de9c
Marharyta Starodubtseva <marharytastarodubtseva@MacBook-Air-Marharyta.local>
2026-05-08 21:51:28 +0300

    muuttaa sovelluksen käyttämään koko ruudun korkeutta

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
+import './assets/styles/main.scss'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
