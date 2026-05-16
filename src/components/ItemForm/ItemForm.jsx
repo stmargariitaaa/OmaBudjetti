857df67c85283e0ff96af13d43150e4e2effee97
Marharyta Starodubtseva <marharytastarodubtseva@MacBook-Air-Marharyta.local>
2026-05-16 21:54:45 +0300

    lisää useFormin lomakekäsittelijäksi

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -1,15 +1,41 @@
+import Button from '../../shared/ui/buttons'
+import useForm from '../../shared/hooks/useform'
 import styles from './ItemForm.module.scss'
 
 function ItemForm(props) {
 
+  // Lomakkeen varsinainen submit-toiminto
+  const submit = () => {
+    console.log(values)
+    alert("SUBMIT")
+  }
+
+  // Lomakkeen alkutila
+  const initialState = {
+    type: "",
+    amount: 0,
+    paymentDate: "",
+    periodStart: "",
+    periodEnd: "",
+    receiver: ""
+  }
+
+  // Haetaan lomakkeen state ja käsittelijät custom-hookista.
+  const {values, handleChange, handleSubmit } = useForm(submit, initialState, false)
+
+  // Peruuta-painikkeen toiminto
+  const handleCancel = () => {
+    alert('CANCEL')
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={styles.itemform}>
           <div className={styles.itemform_row}>
             <div>
               <label htmlFor='type'>Kulutyyppi</label>
-              <select id='type' name='type'>
+              <select id='type' name='type' onChange={handleChange} value={values.type}>
                 <option>Puhelin</option>
                 <option>Sähkö</option>
                 <option>Vesi</option>
@@ -20,27 +46,35 @@ function ItemForm(props) {
           <div className={styles.itemform_row}>
             <div>
               <label htmlFor='amount'>Summa</label>
-              <input id='amount' type='number' name='amount' step='0.01' />
+              <input id='amount' type='number' name='amount' step='0.01' onChange={handleChange} value={values.amount} />
             </div>
             <div>
               <label htmlFor='paymentDate'>Maksupäivä</label>
-              <input id='paymentDate' type='date' name='paymentDate' />
+              <input id='paymentDate' type='date' name='paymentDate' onChange={handleChange} value={values.paymentDate} />
             </div>
           </div>
           <div className={styles.itemform_row}>
             <div>
               <label htmlFor='periodStart'>Laskutuskauden alku</label>
-              <input id='periodStart' type='date' name='periodStart' />
+              <input id='periodStart' type='date' name='periodStart' onChange={handleChange} value={values.periodStart} />
             </div>
             <div>
               <label htmlFor='periodEnd'>Laskutuskauden loppu</label>
-              <input id='periodEnd' type='date' name='periodEnd' />
+              <input id='periodEnd' type='date' name='periodEnd' onChange={handleChange} value={values.periodEnd} />
             </div>
           </div>
           <div className={styles.itemform_row}>
             <div>
               <label htmlFor='receiver'>Saaja</label>
-              <input id='receiver' type='text' name='receiver' />
+              <input id='receiver' type='text' name='receiver' onChange={handleChange} value={values.receiver} />
+            </div>
+          </div>
+          <div className={styles.itemform_row}>
+            <div>
+              <Button onClick={handleCancel}>PERUUTA</Button>
+            </div>
+            <div>
+              <Button primary type='submit'>LISÄÄ</Button>
             </div>
           </div>
         </div>
@@ -49,5 +83,6 @@ function ItemForm(props) {
   )
 
 }
+
 
 export default ItemForm
