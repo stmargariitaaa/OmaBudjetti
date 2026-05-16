d6b843c6dbf94db0dd792b2cb26507c137778c32
Marharyta Starodubtseva <marharytastarodubtseva@MacBook-Air-Marharyta.local>
2026-05-16 22:52:53 +0300

    lisää muokatun merkinnän korvaamisen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,27 @@ function App() {
   const [data, setData] = useState(testdata)
 
   const handleItemSubmit = (newitem) => {
+
+    // Luodaan kopio nykyisestä datasta, jotta statea ei muuteta suoraan.
     let copy = data.slice()
-    newitem.id = crypto.randomUUID()
-    copy.push(newitem)
+    // Etsitään merkintää, jolla on sama id kuin tallennettavalla merkinnällä.
+    const index = copy.findIndex(item => item.id === newitem.id)
+    if (index >= 0) {
+      // Jos rivi löytyi taulukosta, kyseessä on muokkaus →
+      // korvataan vanha uudella
+      copy[index] = newitem
+    } else {
+      // Jos riviä ei löytynyt, kyseessä on lisäys.
+      copy.push(newitem)
+    }
+
+    // Järjestetään rivit maksupäivän mukaan (uusin ensin)
     copy.sort((a, b) => {
       const aDate = new Date(a.paymentDate)
       const bDate = new Date(b.paymentDate)
       return bDate - aDate
     })
+    // Päivitetään sovelluksen state uudella, käsitellyllä datalla.
     setData(copy)
   }
 
