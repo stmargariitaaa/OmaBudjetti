76bcd3621e3ec1a70065d2de1c962f37e3345cb9
Marharyta Starodubtseva <marharytastarodubtseva@MacBook-Air-Marharyta.local>
2026-05-16 23:29:28 +0300

    lisää aikajanan piirtävän viivakaavion

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,10 +1,51 @@
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import styles from './Stats.module.scss'
 
-function Stats() {
+function Stats(props) {
+
+  // Käytettävä lokaaliasetus päivämäärän muotoiluun.
+  const locale = "fi-FI"
+
+  // Luodaan NumberFormat-olio euromääräisten summien esittämiseen.
+  const numberFormat = new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: 'EUR'
+  })
+
+  // Muodostaa props.data-taulukosta uuden taulukon kaaviota varten,
+  // paymentDate muunnetaan aikaleimaksi ja amount otetaan mukaan
+  // sellaisenaan.
+  const linedata = props.data.map(
+    (item) => ({
+      date: new Date(item.paymentDate).getTime(),
+      amount: item.amount
+    })
+  )
 
   return (
     <div className={styles.stats}>
-      <h2>Stats</h2>
+      <h2>Tilastot</h2>
+      <h3>Kulut aikajanalla</h3>
+      <ResponsiveContainer height={350}>
+        <LineChart data={linedata} type='monotone'>
+          <Line dataKey='amount' />
+          <XAxis type='number'
+                 dataKey='date'
+                 domain={['auto','auto']}
+                 scale='time'
+                 minTickGap={32}
+                 tickFormatter={
+                   value => new Date(value).toLocaleDateString(locale)
+                 } />
+          <YAxis />
+          <Tooltip labelFormatter={
+                     value => new Date(value).toLocaleDateString(locale)
+                   }
+                   formatter={
+                     value => [numberFormat.format(value),"maksettu"]
+                   } />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   )
 }
